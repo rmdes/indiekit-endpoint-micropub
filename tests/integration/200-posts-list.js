0465401d168b1d45ad5fff1b1bb40bcc89589127
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-31 22:38:43 +0000

    test: tidy up

--- tests/integration/200-posts-list.js
+++ tests/integration/200-posts-list.js
@@ -4,7 +4,8 @@ import {server} from '@indiekit-test/server';
 
 test('Returns list of previously uploaded files', async t => {
   const request = await server;
-  const response = await request.get('/micropub/posts');
+  const response = await request.get('/micropub/posts')
+    .auth(process.env.TEST_BEARER_TOKEN, {type: 'bearer'});
   const dom = new JSDOM(response.text);
 
   const result = dom.window.document;
